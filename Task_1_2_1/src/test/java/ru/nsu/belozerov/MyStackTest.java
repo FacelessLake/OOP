70b1b5692117883261dc71facb20b8cfa16a8807
FacelessLake <sinonim147@gmail.com>
2021-10-12 19:53:24 +0700

    Add some tests

--- Task_1_2_1/src/test/java/ru/nsu/belozerov/MyStackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/belozerov/MyStackTest.java
@@ -3,7 +3,7 @@ package ru.nsu.belozerov;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Stack;
+import java.util.EmptyStackException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +20,36 @@ class MyStackTest {
         ms2.pushStack(ms);
         ms2.pop();
         ms.popStack(1);
-        Assertions.assertEquals(ms, ms2);
+        assertArrayEquals(ms.arr, ms2.arr);
+    }
+
+    @Test
+    public void Test_MyStackOnString() {
+        MyStack<String> ms = new MyStack<>();
+        ms.push("I");
+        ms.push("Love");
+        ms.push("D&D");
+        Assertions.assertEquals(3, ms.count());
+
+        MyStack<String> ms2 = new MyStack<>();
+        ms2.pushStack(ms);
+        String val1 = ms2.pop();
+        MyStack<String> val = ms.popStack(2);
+        String val2 = val.pop();
+        assertEquals(val2, val1);
+    }
+
+    @Test
+    public void Test_EmptyStack() {
+        MyStack<String> ms = new MyStack<>();
+        Assertions.assertEquals(0, ms.count());
+
+        MyStack<String> ms2 = new MyStack<>();
+        ms2.pushStack(ms);
+        Assertions.assertEquals(0, ms2.count());
+
+        Assertions.assertThrows(EmptyStackException.class, ms::pop);
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> ms.popStack(4));
     }
 }
