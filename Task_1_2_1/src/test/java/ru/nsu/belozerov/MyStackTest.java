f6f49c1b1ff4cd5dd38a771546b1dbbcdafd9452
FacelessLake <sinonim147@gmail.com>
2021-10-12 14:25:58 +0700

    Not finished yet

--- Task_1_2_1/src/test/java/ru/nsu/belozerov/MyStackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/belozerov/MyStackTest.java
@@ -1,7 +1,25 @@
 package ru.nsu.belozerov;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyStackTest {
 
+    @Test
+    public void Test_MyStackOnInt() {
+        MyStack<Integer> ms = new MyStack<>();
+        ms.push(47);
+        ms.push(19);
+        Assertions.assertEquals(2, ms.count());
+
+        MyStack<Integer> ms2 = new MyStack<>();
+        ms2.pushStack(ms);
+        ms2.pop();
+        ms.popStack(1);
+        Assertions.assertEquals(ms, ms2);
+    }
 }
