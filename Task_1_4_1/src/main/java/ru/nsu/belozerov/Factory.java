b814f02baae367de120ad0377fa12ed709a4b983
FacelessLake <sinonim147@gmail.com>
2021-11-16 23:26:11 +0700

    More changes

--- Task_1_4_1/src/main/java/ru/nsu/belozerov/Factory.java
+++ Task_1_4_1/src/main/java/ru/nsu/belozerov/Factory.java
@@ -2,64 +2,51 @@ package ru.nsu.belozerov;
 
 import ru.nsu.belozerov.operation.*;
 
-import java.util.Stack;
-
 public class Factory {
 
-    private static boolean isParsable(String input) {
-        try {
-            Double.parseDouble(input);
-            return true;
-        } catch (final NumberFormatException e) {
-            return false;
-        }
-    }
-
-    public static void operandCheck(Stack<Double> num, String expr) throws IllegalArgumentException {
-        if (isParsable(expr)) {
-            num.push(Double.parseDouble(expr));
-        } else {
-            switch (expr) {
-                case "+": {
-                    new Addition().count(num);
-                    break;
-                }
-                case "-": {
-                    new Subtraction().count(num);
-                    break;
-                }
-                case "*": {
-                    new Multiplication().count(num);
-                    break;
-                }
-                case "/": {
-                    new Division().count(num);
-                    break;
-                }
-                case "sin": {
-                    new Sinus().count(num);
-                    break;
-                }
-                case "cos": {
-                    new Cosine().count(num);
-                    break;
-                }
-                case "sqrt": {
-                    new SquareRoot().count(num);
-                    break;
-                }
-                case "pow": {
-                    new Exponentiation().count(num);
-                    break;
-                }
-                case "log": {
-                    new Logarithm().count(num);
-                    break;
-                }
-                default: {
-                    throw new IllegalArgumentException("Unknown argument: " + expr);
-                }
+    public static Operations operandCheck(String expr) {
+        Operations op;
+        switch (expr) {
+            case "+": {
+                op = new Addition();
+                break;
+            }
+            case "-": {
+                op = new Subtraction();
+                break;
+            }
+            case "*": {
+                op = new Multiplication();
+                break;
+            }
+            case "/": {
+                op = new Division();
+                break;
+            }
+            case "sin": {
+                op = new Sinus();
+                break;
+            }
+            case "cos": {
+                op = new Cosine();
+                break;
+            }
+            case "sqrt": {
+                op = new SquareRoot();
+                break;
+            }
+            case "pow": {
+                op = new Exponentiation();
+                break;
+            }
+            case "log": {
+                op = new Logarithm();
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("Unknown argument: " + expr);
             }
         }
+        return op;
     }
 }
