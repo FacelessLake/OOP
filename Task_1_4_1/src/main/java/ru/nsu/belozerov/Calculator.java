43bbea6afc38970e838bd57fc92ae81c6ad79822
FacelessLake <sinonim147@gmail.com>
2021-11-16 23:26:11 +0700

    More changes

--- Task_1_4_1/src/main/java/ru/nsu/belozerov/Calculator.java
+++ Task_1_4_1/src/main/java/ru/nsu/belozerov/Calculator.java
@@ -1,21 +1,38 @@
 package ru.nsu.belozerov;
 
+import ru.nsu.belozerov.operation.Operations;
+
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Calculator {
     private final Stack<Double> num = new Stack<>();
 
+    private static boolean isParsable(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (final NumberFormatException e) {
+            return false;
+        }
+    }
+
     public double compute(String input) {
         Scanner scanner = new Scanner(input);
         return compute(scanner);
     }
 
-    public double compute(Scanner scanner) {
+    public double compute(Scanner scanner) throws IllegalArgumentException {
         String string = scanner.nextLine();
         String[] words = string.replaceAll("\\s+", " ").split(" ");
+        Operations operation;
         for (int i = words.length - 1; i >= 0; i--) {
-            Factory.operandCheck(num, words[i]);
+            if (isParsable(words[i])) {
+                num.push(Double.parseDouble(words[i]));
+            } else {
+                operation = Factory.operandCheck(words[i]);
+                operation.count(num);
+            }
         }
         return num.pop();
     }
