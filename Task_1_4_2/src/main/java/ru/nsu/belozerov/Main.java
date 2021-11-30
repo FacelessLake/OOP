6605a0c016934d9323ab12aa74fc8a817206583d
FacelessLake <sinonim147@gmail.com>
2021-11-30 20:35:42 +0700

    Works with other files

--- Task_1_4_2/src/main/java/ru/nsu/belozerov/Main.java
+++ Task_1_4_2/src/main/java/ru/nsu/belozerov/Main.java
@@ -2,14 +2,7 @@ package ru.nsu.belozerov;
 
 public class Main {
     public static void main(String[] args) {
-       String[] args2 = new String[1];
-//       args2[0] = "-rm";
-//       args2[1] = "memes2";
-//        args2[0] = "-add";
-//        args2[2] = "want to try some more memes";
-//        args2[0] = "-show";
-        args2[0] = "-help";
         ConsoleReader cr = new ConsoleReader();
-        cr.run(args2);
+        cr.run(args);
     }
 }
