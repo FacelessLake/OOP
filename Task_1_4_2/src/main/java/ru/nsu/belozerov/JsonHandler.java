fc90b8fac8d28fec411fd32b2ef77492ba176d56
FacelessLake <sinonim147@gmail.com>
2021-11-30 21:09:14 +0700

    Added javadocs

--- Task_1_4_2/src/main/java/ru/nsu/belozerov/JsonHandler.java
+++ Task_1_4_2/src/main/java/ru/nsu/belozerov/JsonHandler.java
@@ -11,6 +11,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/**
+ * This class is responsible for work with files
+ */
 public class JsonHandler {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
     private final Gson gson = new GsonBuilder()
@@ -25,28 +28,66 @@ public class JsonHandler {
     private final File file;
     private Reader reader = null;
     private FileWriter writer = null;
+
+
+    /**
+     * Takes the name of the file
+     *
+     * @param filename - the name of the file that will be used
+     */
     public JsonHandler(File filename) {
         file = filename;
     }
 
+
+    /**
+     * Opens file for reading
+     *
+     * @throws IOException - will be thrown if something will go wrong with file
+     */
     public void openRead() throws IOException {
         file.createNewFile();
         reader = Files.newBufferedReader(file.toPath());
     }
 
+
+    /**
+     * Opens file for writing
+     *
+     * @throws IOException - will be thrown if something will go wrong with file
+     */
     public void openWrite() throws IOException {
         writer = new FileWriter(file, false);
     }
 
+
+    /**
+     * Closes file for both reading and writing
+     *
+     * @throws IOException - will be thrown if something will go wrong with file
+     */
     public void close() throws IOException {
         reader.close();
         writer.close();
     }
 
+
+    /**
+     * Read from .json file
+     *
+     * @return - array of the notes
+     */
     public Note[] readJson() {
         return gson.fromJson(reader, Note[].class);
     }
 
+
+    /**
+     * Write into the .json file
+     *
+     * @param notes - notes that will be writen
+     * @throws IOException - will be thrown if something will go wrong with file
+     */
     public void writeJson(Notebook notes) throws IOException {
         writer = new FileWriter(file, false);
         gson.toJson(notes.getNotes(), writer);
