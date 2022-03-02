feff1346503813089953267c63cfd7a4d54cae7b
FacelessLake <sinonim147@gmail.com>
2022-03-03 00:52:55 +0700

    Added new fixes, added javadocs

--- Task_2_1_1/src/main/java/ru/nsu/belozerov/PrimeNumberParallelStream.java
+++ Task_2_1_1/src/main/java/ru/nsu/belozerov/PrimeNumberParallelStream.java
@@ -3,7 +3,15 @@ package ru.nsu.belozerov;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/**
+ * Class for prime number check using threads using parallel streams
+ */
 public class PrimeNumberParallelStream {
+    /**
+     * Checks if an array contains a number that is NOT prime using parallel streams
+     * @param arr - array you want to check
+     * @return true - if it contains NOT prime number, false - otherwise
+     */
     static public boolean parallelCheck(int[] arr) {
         IntStream stream = Arrays.stream(arr);
         return stream.parallel().anyMatch(PrimeNumber::checkNotPrime);
--- Task_2_1_1/src/test/java/ru/nsu/belozerov/PrimeNumberTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/belozerov/PrimeNumberTest.java
@@ -2,11 +2,13 @@ package ru.nsu.belozerov;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeNumberTest {
+    private static final Random random = new Random();
 
     @Test
     void checkArray_true() {
@@ -16,7 +18,7 @@ class PrimeNumberTest {
 
     @Test
     void checkPrimeThreads_true() throws ExecutionException, InterruptedException {
-        Integer[] arr = {6, 8, 7, 13, 9, 4};
+        int[] arr = {6, 8, 7, 13, 9, 4};
         assertTrue(PrimeNumberThreads.checkPrimeThreads(arr));
     }
 
@@ -34,7 +36,7 @@ class PrimeNumberTest {
 
     @Test
     void checkPrimeThreads_false() throws ExecutionException, InterruptedException {
-        Integer[] arr = {11, 3, 5, 7};
+        int[] arr = {11, 3, 5, 7};
         assertFalse(PrimeNumberThreads.checkPrimeThreads(arr));
     }
 
@@ -52,7 +54,7 @@ class PrimeNumberTest {
 
     @Test
     void checkPrimeThreads_withZeroesAndOnes() throws ExecutionException, InterruptedException {
-        Integer[] arr = {0, 1};
+        int[] arr = {0, 1};
         assertTrue(PrimeNumberThreads.checkPrimeThreads(arr));
     }
 
@@ -70,7 +72,7 @@ class PrimeNumberTest {
 
     @Test
     void checkPrimeThreads_bigNumbers() throws ExecutionException, InterruptedException {
-        Integer[] arr = {6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053};
+        int[] arr = {6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053};
         assertFalse(PrimeNumberThreads.checkPrimeThreads(arr));
     }
 
@@ -78,5 +80,35 @@ class PrimeNumberTest {
     void checkParallelStreams_bigNumbers() {
         int[] arr = {6997901, 6997927, 6997937, 6997967, 6998009, 6998029, 6998039, 6998051, 6998053};
         assertFalse(PrimeNumberParallelStream.parallelCheck(arr));
+    }
+
+    @Test
+    void checkArray_notPrimeAtTheEnd() {
+        int[] arr = {3, 3, 7, 7, 3, 4};
+        assertTrue(PrimeNumber.checkArray(arr));
+    }
+
+    @Test
+    void checkPrimeThreads_notPrimeAtTheEnd() throws ExecutionException, InterruptedException {
+        int[] arr = {3, 3, 7, 7, 3, 4};
+        assertTrue(PrimeNumberThreads.checkPrimeThreads(arr));
+    }
+
+    @Test
+    void checkParallelStreams_notPrimeAtTheEnd() {
+        int[] arr = {3, 3, 7, 7, 3, 4};
+        assertTrue(PrimeNumberParallelStream.parallelCheck(arr));
+    }
+
+    @Test
+    void checkPrimeThreads_random() throws ExecutionException, InterruptedException {
+        int[] arr = random.ints(10000, 0, Integer.MAX_VALUE).toArray();
+        assertEquals(PrimeNumber.checkArray(arr), PrimeNumberThreads.checkPrimeThreads(arr));
+    }
+
+    @Test
+    void checkParallelStreams_random() {
+        int[] arr = random.ints(10000, 0, Integer.MAX_VALUE).toArray();
+        assertEquals(PrimeNumber.checkArray(arr), PrimeNumberParallelStream.parallelCheck(arr));
     }
 }
