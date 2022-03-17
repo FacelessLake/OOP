c80261bae4359b43dcf15e9317443c6ad6b6fce9
FacelessLake <sinonim147@gmail.com>
2022-03-17 13:54:37 +0700

    It works, but code is duplicated

--- Task_2_2_1/src/main/java/ru/nsu/belozerov/ProducerConsumer.java
+++ Task_2_2_1/src/main/java/ru/nsu/belozerov/ProducerConsumer.java
@@ -3,21 +3,23 @@ package ru.nsu.belozerov;
 import java.util.Random;
 
 public class ProducerConsumer implements Runnable {
-    private final DataQueue producerQueue;
+    public final DataQueue producerQueue;
     private final DataQueue consumerQueue;
-    private volatile boolean runFlag;
+    private volatile boolean runProducerFlag;
+    private volatile boolean runConsumerFlag;
     private int orderCounter;
     private final String orderProduceStatus;
     private final String orderConsumeStatus;
-    private int processingTime = 0;
-    private final Random random = new Random();
+    public int processingTime = 0;
+    public final Random random = new Random();
 
-    public ProducerConsumer(DataQueue consumerQueue, DataQueue producerQueue, String orderProduceStatus, String orderConsumeStatus) {
+    public ProducerConsumer(DataQueue consumerQueue, DataQueue producerQueue, String orderConsumeStatus, String orderProduceStatus) {
         this.producerQueue = producerQueue;
         this.consumerQueue = consumerQueue;
         this.orderProduceStatus = orderProduceStatus;
         this.orderConsumeStatus = orderConsumeStatus;
-        runFlag = true;
+        runConsumerFlag = true;
+        runProducerFlag = true;
         orderCounter = 0;
     }
 
@@ -26,19 +28,19 @@ public class ProducerConsumer implements Runnable {
         this.consumerQueue = dataQueue;
         this.orderProduceStatus = orderStatus;
         this.orderConsumeStatus = orderStatus;
-        runFlag = true;
+        runConsumerFlag = true;
+        runProducerFlag = true;
         orderCounter = 0;
     }
 
     @Override
     public void run() {
-        consume();
         produce();
     }
 
     @SuppressWarnings("BusyWait")
     public void produce() {
-        while (runFlag) {
+        while (runProducerFlag) {
             Order order = generateOrder();
             while (producerQueue.isFull()) {
                 try {
@@ -47,50 +49,105 @@ public class ProducerConsumer implements Runnable {
                     break;
                 }
             }
-            if (!runFlag) {
+            if (!runProducerFlag) {
                 break;
             }
             producerQueue.add(order);
             producerQueue.notifyAllForEmpty();
             try {
-                Thread.sleep(this.random.nextInt(this.processingTime));
+                Thread.sleep(random.nextInt(processingTime));
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                if (!runProducerFlag) {
+                    break;
+                }
             }
         }
     }
 
     @SuppressWarnings("BusyWait")
     public void consume() {
-        while (runFlag) {
-            Order order;
+        while (runConsumerFlag) {
             if (consumerQueue.isEmpty()) {
                 try {
                     consumerQueue.waitOnEmpty();
-                } catch (InterruptedException e) {
-                    break;
+                } catch (InterruptedException ignored) {
                 }
             }
-            if (!runFlag) {
+            if (!runConsumerFlag) {
                 break;
             }
-            order = consumerQueue.remove();
-            consumerQueue.notifyAllForFull();
+            Order order = consumerQueue.remove();
             changeOrderStatus(order);
+            consumerQueue.notifyAllForFull();
             try {
-                Thread.sleep(this.random.nextInt(this.processingTime));
+                Thread.sleep(random.nextInt(processingTime));
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                if (!runConsumerFlag) {
+                    break;
+                }
             }
         }
     }
 
-    private Order generateOrder() {
+    @SuppressWarnings("BusyWait")
+    public void consumeProduce() {
+        while (runConsumerFlag) {
+            if (consumerQueue.isEmpty()) {
+                try {
+                    consumerQueue.waitOnEmpty();
+                } catch (InterruptedException ignored) {
+                }
+            }
+            if (!runConsumerFlag) {
+                break;
+            }
+            Order order = consumerQueue.remove();
+            changeOrderStatus(order);
+            consumerQueue.notifyAllForFull();
+            try {
+                Thread.sleep(random.nextInt(processingTime));
+            } catch (InterruptedException e) {
+                if (!runConsumerFlag) {
+                    break;
+                }
+            }
+            Order delivery = generateDelivery(order.getOrderNumber());
+            while (producerQueue.isFull()) {
+                try {
+                    producerQueue.waitOnFull();
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+            if (!runProducerFlag) {
+                break;
+            }
+            producerQueue.add(delivery);
+            producerQueue.notifyAllForEmpty();
+            try {
+                Thread.sleep(random.nextInt(processingTime));
+            } catch (InterruptedException e) {
+                if (!runProducerFlag) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public Order generateOrder() {
         Order order = new Order();
         orderCounter++;
         order.setOrderStatus(orderProduceStatus);
         order.setOrderNumber(orderCounter);
         System.out.println("Order[" + orderCounter + "] is " + orderProduceStatus);
+        return order;
+    }
+
+    public Order generateDelivery(int deliveryCounter) {
+        Order order = new Order();
+        order.setOrderStatus(orderProduceStatus);
+        order.setOrderNumber(deliveryCounter);
+        System.out.println("Order[" + deliveryCounter + "] is " + orderProduceStatus);
         return order;
     }
 
@@ -104,12 +161,12 @@ public class ProducerConsumer implements Runnable {
     }
 
     public void stopProduce() {
-        runFlag = false;
+        runProducerFlag = false;
         producerQueue.notifyAllForFull();
     }
 
     public void stopConsume() {
-        runFlag = false;
+        runConsumerFlag = false;
         consumerQueue.notifyAllForEmpty();
     }
 }
