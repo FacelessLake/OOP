6f72f626ddf33528d3272d1e9b93f634e4e2a29f
FacelessLake <sinonim147@gmail.com>
2022-03-30 17:18:54 +0700

    Fixed additional pizzas problem

--- Task_2_2_1/src/main/java/ru/nsu/belozerov/Producer.java
+++ Task_2_2_1/src/main/java/ru/nsu/belozerov/Producer.java
@@ -1,10 +1,10 @@
 package ru.nsu.belozerov;
 
-public interface Producer<T> extends Runnable {
+public interface Producer extends Runnable {
 
     void run();
 
-    void producer(T order);
+    void producer();
 
     void stopProduce();
 
